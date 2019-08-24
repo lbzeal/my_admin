ced1de5c3402d606af574ff9f6154251e3cb8aa2
liubiao <1145037541@qq.com>
2019-08-24 23:40:00 +0800

    last

--- be/controller/user.js
+++ be/controller/user.js
@@ -1,10 +1,19 @@
 const emailer = require('../utils/email');
 
 module.exports = {
-    sendMa(req,res,next){
-        let ma_content = '1234' 
-        let email_address = req.body.email;
-        emailer.sendEmail(email_address,ma_content);
+    resolveMa(req,res,next){
+        let req_data = req.body;
+        let req_arr = Object.keys(req_data);
+        let ma_content = '';
+        if(req_arr.length == 1){
+            ma_content  = '1234'; 
+            let email_address = req_data.email;
+            emailer.sendEmail(email_address,ma_content);
+        }
+        else{
+            
+        }
+       
    
     }
 }
--- be/routes/users.js
+++ be/routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const users = require('../controller/user');
 
 /* GET users listing. */
-router.post('/email', users.sendMa);
+router.post('/email', users.resolveMa);
 // router.post('/register',users.confirmMa,users.register)
 
 module.exports = router;
